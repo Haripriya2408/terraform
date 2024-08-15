b3032a555320f4bed88d02191a2b0450c8b042a7
Haripriya Velchamy <112542675+Haripriya2408@users.noreply.github.com>
2024-08-15 21:07:53 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,8 @@
+provider "aws" {
+    region = "ap-southeast-2"  # Set your desired AWS region
+}
+
+resource "aws_instance" "example" {
+    ami           = ""  # Specify an appropriate AMI ID
+    instance_type = "t2.micro"
+}
